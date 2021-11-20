e80a2fecc4ab71725c777ec07ce6583fd320ada0
Yueyi Wang <yxw835@cs.bham.ac.uk>
2021-11-20 14:31:32 +0000

    master

---
+++ len_of_last_world_58.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int n = s.size();
+        if(n<=0) return 0;
+
+        int res = 0;
+        for (int i = n-1; i>=0 ; --i)
+        {
+            if(s[i]==' '){
+                if (res != 0){
+                    return res;
+                }
+                continue;
+            }else{
+                res+=1;
+            }
+        }
+        return res;
+    }
+};
