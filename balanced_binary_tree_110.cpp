2c54b2a2c9474737516ed63abb58190a5dead1e2
66Kevin <YXW835@bham.ac.uk>
2022-01-20 21:02:48 +0000

    add

---
+++ balanced_binary_tree_110.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 王月艺 on 2022/1/20.
+//
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// 递归思想，判断当前节点是否平衡的同时，还要确保当前节点的左右节点是否平衡
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        } else {
+            return max(height(root->left), height(root->right)) + 1;
+        }
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        } else {
+            return abs(height(root->left) - height(root->right)) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+        }
+    }
+};
