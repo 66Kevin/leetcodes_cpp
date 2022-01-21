8d110590533a1b8db329462fbc30243a398b8f33
66Kevin <YXW835@bham.ac.uk>
2022-01-21 16:08:02 +0000

    add

---
+++ min_depth_111.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int getDepth(TreeNode* node) {
+        if (node == NULL) return 0;
+        int leftDepth = getDepth(node->left);           // 左
+        int rightDepth = getDepth(node->right);         // 右
+
+        // 当一个左子树为空，右不为空，这时并不是最低点
+        if (node->left == NULL && node->right != NULL) {
+            return 1 + rightDepth;
+        }
+        // 当一个右子树为空，左不为空，这时并不是最低点
+        if (node->left != NULL && node->right == NULL) {
+            return 1 + leftDepth;
+        }
+        return 1 + min(leftDepth, rightDepth);
+    }
+
+    int minDepth(TreeNode* root) {
+        return getDepth(root);
+    }
+};
