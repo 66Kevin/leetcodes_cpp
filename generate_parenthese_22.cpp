52f81b5170c37a138ea0fbf00f5f15900fc6b600
66Kevin <YXW835@bham.ac.uk>
2022-02-03 21:01:48 +0000

    add

---
+++ generate_parenthese_22.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 王月艺 on 2022/2/3.
+//
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> result;
+    vector<string> generateParenthesis(int n) {
+        backtracking(n, 0, 0, "");
+        return result;
+    }
+    void backtracking(int n, int left, int right, string str){
+        if(right > left){
+            return;
+        }
+        if(right == left == n){
+            result.push_back(str);
+            return;
+        }
+        if(left > right){
+            backtracking(n, left, right+1, str+")");
+        }
+        if(left < n){
+            backtracking(n, left+1, right, str+"(");
+        }
+    }
+};
+
+int main(){
+    Solution sol;
+    sol.generateParenthesis(2);
+
+}
