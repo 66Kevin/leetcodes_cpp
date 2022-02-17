ba86f86229947721e0843515af1fce010fabc9b9
66Kevin <YXW835@bham.ac.uk>
2022-02-17 17:02:49 +0000

    add

---
+++ lemonChange_860.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 王月艺 on 2022/2/17.
+//
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for (auto& bill: bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
