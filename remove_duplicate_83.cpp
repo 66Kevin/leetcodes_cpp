c4fec5d176fec7f1eb8e77b265fdf46e5a4e195c
66Kevin <YXW835@bham.ac.uk>
2022-03-04 15:03:11 +0000

    add

---
+++ remove_duplicate_83.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 王月艺 on 2022/3/4.
+//
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) {
+            return head;
+        }
+
+        ListNode* cur = head;
+        while (cur->next) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            }
+            else {
+                cur = cur->next;
+            }
+        }
+
+        return head;
+    }
+};
