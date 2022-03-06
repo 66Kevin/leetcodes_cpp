0120e727c0e1f2fac88d6d3c24d4eaf369a3741a
66Kevin <YXW835@bham.ac.uk>
2022-03-06 15:38:14 +0000

    add

---
+++ canFormArray_1640.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 王月艺 on 2022/3/6.
+//
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        unordered_map<int, int> dic;
+        for (int i = 0; i < pieces.size(); i++) {
+            dic[pieces[i][0]] = i;
+        }
+
+        for (int i = 0; i < arr.size();) {
+            if (dic.find(arr[i]) == dic.end()) return false;
+            auto& p = pieces[dic[arr[i]]];
+            for (int j = 0; j < p.size(); j++, i++) {
+                if (arr[i] != p[j]) return false;
+            }
+        }
+        return true;
+    }
+};
