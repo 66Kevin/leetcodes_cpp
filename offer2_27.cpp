402013bc3cf9e4bae635215bfd6412aaf5b639e1
66Kevin <YXW835@bham.ac.uk>
2022-05-10 14:29:18 +0100

    update

---
+++ offer2_27.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    TreeNode* mirrorTree(TreeNode* root) {
+        if (root == nullptr) return nullptr;
+        TreeNode* tmp = root->left;
+        root->left = mirrorTree(root->right);
+        root->right = mirrorTree(tmp);
+        return root;
+    }
+};
+
