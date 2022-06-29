1efc280cb2eba277ba2871662f081254ac7a1ece
66Kevin <YXW835@bham.ac.uk>
2022-06-29 13:21:48 +0100

    update

---
+++ swap_nodes_24.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    //迭代
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* temp = dummyHead;
+        
+        while (temp->next != nullptr && temp->next->next != nullptr) {
+            ListNode* node1 = temp->next;
+            ListNode* node2 = temp->next->next;
+            temp->next = node2;
+            node1->next = node2->next;
+            node2->next = node1;
+            temp = node1;
+        }
+        return dummyHead->next;
+    }
+};
